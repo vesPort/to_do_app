01aff185443ebf7cfd2d1442b5b3000b6d6780a2
Valerii Chernikov <chernikov2702@gmail.com>
2023-01-08 13:10:00 +0200

    _init_

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Fragment, useState } from "react";
+import { AiFillCheckCircle, AiFillEdit } from "react-icons/ai";
+import { BsFillTrashFill } from "react-icons/bs";
+import "./App.css";
 
 function App() {
+  //ToDos
+  const [toDo, setToDo] = useState([
+    {
+      id: 1,
+      title: "Task1",
+      status: false,
+    },
+    {
+      id: 2,
+      title: "Task2",
+      status: false,
+    },
+  ]);
+  //Temp State
+  const [newTask, setNewTask] = useState("");
+  const [editTask, setEditTask] = useState("");
+
+  //Add Task
+  const addTask = (task) => {};
+  //Edit Task
+  const editTaskData = (task) => {};
+  //Delete Task
+  const deleteTask = (id) => {};
+  //Task Done or COmpleted
+  const markCompleteOrDone = (id) => {};
+  //Cncel Update
+  const cancelUpdate = () => {};
+  //Chnge TAsk for update
+  const changeTask = () => {};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container App">
+      ToDoApp
+      {/* {Display ToDOs} */}
+      {toDo && toDo.length ? "" : "No Tasks"}
+      {toDo &&
+        toDo
+        .sort((objA,objB) => objA.id > objB ? 1 : -1).map((task, index) => {
+          return (
+            <Fragment key={index}>
+              <div className="col taskBg">
+                <div className={task.status ? "done" : ""}>
+                  <span className="tasktext">{task.title}</span>
+                </div>
+                <div className="iconsWrap">
+                  <span title="Completed / Uncomleted"><AiFillCheckCircle/></span>
+                  <span title="Edit"><AiFillEdit/></span>
+                  <span title="Delete"><BsFillTrashFill/></span>
+
+                </div>
+              </div>
+            </Fragment>
+          );
+        })}
     </div>
   );
 }
