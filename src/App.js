e7a7ced865889b6194bc8d36fe9f4886cb6acba7
Valerii Chernikov <chernikov2702@gmail.com>
2023-01-09 10:58:05 +0200

    Added deliting, add, updating todos functionality

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
   //ToDos
   const [toDo, setToDo] = useState([
     {
-      id: 1,
+      id: 0,
       title: "Task1",
       status: false,
     },
     {
-      id: 2,
+      id: 1,
       title: "Task2",
       status: false,
     },
@@ -22,26 +22,99 @@ function App() {
   const [editTask, setEditTask] = useState("");
 
   //Add Task
-  const addTask = (task) => {};
+  const addTask = () => {
+    if (newTask) {
+      let num = toDo.length + 1;
+      let newEntry = { id: num, title: newTask, status: false };
+      setToDo([...toDo, newEntry]);
+      setNewTask("");
+    }
+  };
   //Edit Task
-  const editTaskData = (task) => {};
+  const editTaskData = (task) => {
+    let filterRecord = [...toDo].filter((task) => task.id !== editTask.id);
+    let updatedObj = [...filterRecord, editTask];
+    setToDo(updatedObj);
+    setEditTask("");
+  };
   //Delete Task
-  const deleteTask = (id) => {};
+  const deleteTask = (taskId) => {
+    let newTasks = toDo.filter((item) => item.id !== taskId);
+    setToDo(newTasks);
+  };
   //Task Done or COmpleted
-  const markCompleteOrDone = (id) => {};
+  const markCompleteOrNot = (id) => {
+    let task = toDo.map((task) => {
+      if (task.id === id) {
+        return {
+          ...task,
+          status: !task.status,
+        };
+      }
+      return task;
+    });
+    setToDo(task);
+  };
   //Cncel Update
-  const cancelUpdate = () => {};
+  const cancelUpdate = () => {
+    setEditTask("");
+  };
   //Chnge TAsk for update
-  const changeTask = () => {};
+  const changeTask = (e) => {
+    let newEntry = {
+      id: editTask.id,
+      title: e.target.value,
+      status: editTask.status,
+    };
+    setEditTask(newEntry);
+  };
 
   return (
     <div className="container App">
       ToDoApp
+      <br></br>
+      {/* Update Task */}
+      {editTask && editTask ? (
+        <div className="row">
+          <div className="col">
+            <input
+              value={editTask && editTask.title}
+              onChange={(e) => changeTask(e)}
+              className="form-control form-control-lg"
+            />
+          </div>
+          <div className="col-auto">
+            <button
+              className="btn btn-lg button-success mr-20"
+              onClick={editTaskData}
+            >
+              Update
+            </button>
+            <button className="btn btn-lg button-warning" onClick={cancelUpdate}>Cancel</button>
+          </div>
+        </div>
+      ) : (
+        <div className="row">
+          <div className="col">
+            <input
+              className="form-control form-control-lg"
+              value={newTask}
+              onChange={(event) => setNewTask(event.target.value)}
+            />
+          </div>
+          <div className="col-auto">
+            <button className="btn btn-lg btn-success" onClick={addTask}>
+              Add Task
+            </button>
+          </div>
+          <br></br>
+        </div>
+      )}
+      {/* Add Task */}
       {/* {Display ToDOs} */}
       {toDo && toDo.length ? "" : "No Tasks"}
       {toDo &&
-        toDo
-        .sort((objA,objB) => objA.id > objB ? 1 : -1).map((task, index) => {
+        toDo.map((task, index) => {
           return (
             <Fragment key={index}>
               <div className="col taskBg">
@@ -49,10 +122,31 @@ function App() {
                   <span className="tasktext">{task.title}</span>
                 </div>
                 <div className="iconsWrap">
-                  <span title="Completed / Uncomleted"><AiFillCheckCircle/></span>
-                  <span title="Edit"><AiFillEdit/></span>
-                  <span title="Delete"><BsFillTrashFill/></span>
-
+                  <span
+                    title="Completed / Uncomleted"
+                    onClick={() => markCompleteOrNot(task.id)}
+                  >
+                    <AiFillCheckCircle />
+                  </span>
+                  {task.status ? (
+                    ""
+                  ) : (
+                    <span
+                      title="Edit"
+                      onClick={() =>
+                        setEditTask({
+                          id: task.id,
+                          title: task.title,
+                          status: task.status,
+                        })
+                      }
+                    >
+                      <AiFillEdit />
+                    </span>
+                  )}
+                  <span title="Delete" onClick={() => deleteTask(task.id)}>
+                    <BsFillTrashFill />
+                  </span>
                 </div>
               </div>
             </Fragment>
